6f14f48d0417d536c027556807d12895c0e1735b
Eugenio Miró <eugenio.miro@neoris.com>
2019-08-16 15:23:13 -0300

    creacion de listas iniciales

--- websiteupdater/websiteupdater.ps1
+++ websiteupdater/websiteupdater.ps1
@@ -1,3 +1,23 @@
 ﻿#
 # Script.ps1
 #
+# Sample command line:
+#  .\websiteupdater.ps1 -source D:\eugenio\deploys\PosPre\ -destination D:\eugenio\deploys\Refactor\
+#
+param(
+	[string] $source,
+	[string] $destination
+)
+
+echo "source is      : $source"
+echo "destination is : $destination"
+
+echo "Building source list..."
+$sourceFileList = Get-ChildItem -path $source -Recurse | % { $_.FullName.Replace($source, "") }
+echo "found $($sourceFileList.Count) files" 
+
+echo "Building destination list..."
+$destinationFileList = Get-ChildItem -path $destination -Recurse | % { $_.FullName.Replace($destination, "") }
+echo "found $($destinationFileList.Count) files" 
+
+echo "Backing up destination"
