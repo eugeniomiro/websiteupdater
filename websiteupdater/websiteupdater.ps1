6e9ee646141100acaf1621a218d8bab5f15f9b91
Eugenio Miró <eugenio.miro@neoris.com>
2019-08-17 09:28:47 -0300

    compare files between folders include readme file in project

--- websiteupdater/websiteupdater.ps1
+++ websiteupdater/websiteupdater.ps1
@@ -9,6 +9,23 @@ param(
 	[string] $destination
 )
 
+function IsSameContent($firstFile, $secondFile)
+{
+	$i = 0
+	if ($firstFile.length -ne $secondFile.length) {
+		$false
+		return
+	}
+	while($i -lt $firstFile.length) {
+		if ($firstFile[$i] -ne $secondFile[$i]) {
+			$false
+			return
+		}
+		$i++
+	}
+	$true
+}
+
 echo "source is      : $source"
 echo "destination is : $destination"
 
@@ -19,5 +36,32 @@ echo "found $($sourceFileList.Count) files"
 echo "Building destination list..."
 $destinationFileList = Get-ChildItem -path $destination -Recurse | % { $_.FullName.Replace($destination, "") }
 echo "found $($destinationFileList.Count) files" 
+
+$newFiles = $sourceFileList | where { !$destinationFileList.Contains($_) }
+echo "found $($newFiles.Count) new files"
+$newFiles
+
+$removedFiles = $destinationFileList | where { !$sourceFileList.Contains($_) }
+echo "found $($removedFiles.Count) removed files"
+$removedFiles
+
+$differentFiles = $destinationFileList | 
+	where { $sourceFileList.Contains($_) -and $($(Get-Item $(join-Path -Path $source -ChildPath $_)) -isnot [System.IO.DirectoryInfo]) } | 
+	where { 
+		$allBytesSource = [System.IO.File]::ReadAllBytes($(join-Path -Path $source -ChildPath $_))
+		$allBytesDestination = [System.IO.File]::ReadAllBytes($(join-Path -Path $destination -ChildPath $_))
+		!$(IsSameContent -firstFile $allBytesSource -secondFile $allBytesDestination)
+	}
+
+	#where { Compare-Object -ReferenceObject (Get-Content $(join-Path -Path $source -ChildPath $_)) -DifferenceObject (Get-Content $(Join-Path -Path $destination -ChildPath $_)) }
+	#where { 
+	#	Write-Host $_;
+	#	$result = $(Get-Item $(join-Path -Path $source -ChildPath $_)) -isnot [System.IO.DirectoryInfo] -and `
+	#			  ($(Get-Content $(join-Path -Path $source -ChildPath $_)) -eq $(Get-Content $(Join-Path -Path $destination -ChildPath $_)))
+	#	$result
+	#}
+$differentFiles
+echo "found $($differentFiles.Count) different files"
+
 
 echo "Backing up destination"
