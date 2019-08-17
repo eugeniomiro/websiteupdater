4098572d6ee390c8b2613e6cc2d1297eac2d6143
Eugenio Miró <eugenio.miro@neoris.com>
2019-08-17 16:50:47 -0300

    generate folder with different and new files and a file with a list of files to remove from the destination

--- websiteupdater/websiteupdater.ps1
+++ websiteupdater/websiteupdater.ps1
@@ -2,66 +2,127 @@
 # Script.ps1
 #
 # Sample command line:
-#  .\websiteupdater.ps1 -source D:\eugenio\deploys\Refactor\ -destination D:\eugenio\deploys\PosPre\ 
+#  .\websiteupdater.ps1 -source D:\eugenio\deploys\Refactor\ -destination D:\eugenio\deploys\PosPre\ [-baseFolderName BACKEND]
 #
+[CmdletBinding(SupportsShouldProcess=$true)]
 param(
 	[string] $source,
-	[string] $destination
+	[string] $destination,
+	[string] $baseFolderName = "FRONTEND"
 )
 
-function IsSameContent($firstFile, $secondFile)
+function IsSameContent([byte[]] $firstFile, [byte[]] $secondFile)
 {
-	$i = 0
-	if ($firstFile.length -ne $secondFile.length) {
-		$false
-		return
-	}
-	while($i -lt $firstFile.length) {
-		if ($firstFile[$i] -ne $secondFile[$i]) {
-			$false
-			return
-		}
-		$i++
-	}
-	$true
+	$enum1 = $firstFile.GetEnumerator()
+    $enum2 = $secondFile.GetEnumerator()    
+
+    while ($enum1.MoveNext() -and $enum2.MoveNext()) {
+      if ($enum1.Current -ne $enum2.Current) {
+            return $false
+      }
+    }
+    return $true
 }
 
-echo "source is      : $source"
-echo "destination is : $destination"
+function CreateFolderWithSlots([string] $baseName)
+{
+	$iteration = 0
+	$tentativeFolderName = $baseName
+	$foundSlot = $false
 
-echo "Building source list..."
+	do {		
+		if (!$(Test-Path $tentativeFolderName))
+		{
+			$foundSlot = $true
+			Write-Verbose "Backing up to $tentativeFolderName"
+		}
+		else
+		{
+			Write-Verbose "$tentativeFolderName already exists"
+			$iteration = $iteration + 1
+			$tentativeFolderName = "$($baseName)_$iteration"
+			Write-Verbose "Switching to $tentativeFolderName"
+		}
+	} while (!$foundSlot)
+	md $tentativeFolderName
+	$tentativeFolderName
+}
+
+function BackupFiles([string] $source)
+{
+	Write-Verbose "Backing up $source" 
+	
+	$backupFolder = CreateFolderWithSlots -baseName "Backup"
+	Copy-Item -Path $source -Destination $backupFolder
+}
+
+function copy-file-creating-folder-if-needed([string] $source, [string] $destination)
+{
+	$destinationDir = [system.io.Path]::GetDirectoryName($destination)
+	if (!$(Test-Path $destinationDir)) {
+		Write-Verbose "creating destinationDir = $destinationDir"
+		md $destinationDir | Out-Null
+	}
+	Write-Verbose "copying $source to $destinationFile"
+	Copy-Item -Path $source -Destination $destinationFile -Recurse
+}
+
+$frontendFolder = CreateFolderWithSlots -baseName $baseFolderName
+
+echo "new files are in      : $source"
+echo "old files are in      : $destination"
+echo "destination folder is : $frontendFolder"
+
+Write-Verbose -Message "Building source list..."
 $sourceFileList = Get-ChildItem -path $source -Recurse | % { $_.FullName.Replace($source, "") }
-echo "found $($sourceFileList.Count) files" 
+Write-Verbose -Message "Source Files`n`t$([System.String]::Join("`n`r`t", $sourceFileList))"
+Write-Verbose -Message "found $($sourceFileList.Count) files" 
+Write-Verbose -Message ""
 
-echo "Building destination list..."
+Write-Verbose "Building destination list..."
 $destinationFileList = Get-ChildItem -path $destination -Recurse | % { $_.FullName.Replace($destination, "") }
-echo "found $($destinationFileList.Count) files" 
+Write-Verbose -Message "Destination Files`n`t$([System.String]::Join("`n`r`t", $destinationFileList))"
+Write-Verbose -Message "found $($destinationFileList.Count) files" 
+Write-Verbose -Message ""
 
 $newFiles = $sourceFileList | where { !$destinationFileList.Contains($_) }
-echo "found $($newFiles.Count) new files"
-$newFiles
+Write-Verbose -Message "Destination Files`n`t$([System.String]::Join("`n`r`t", $newFiles))"
+Write-Verbose -Message "found $($newFiles.Count) new files"
+Write-Verbose -Message ""
 
-$removedFiles = $destinationFileList | where { !$sourceFileList.Contains($_) }
-echo "found $($removedFiles.Count) removed files"
-$removedFiles
+$filesToRemove = $destinationFileList | where { !$sourceFileList.Contains($_) }
+Write-Verbose -Message "Files to remove"
+Write-Verbose -Message "Destination Files`n`t$([System.String]::Join("`n`r`t", $filesToRemove))"
+Write-Verbose -Message "found $($filesToRemove.Count) files to remove"
+Write-Verbose -Message ""
 
+Write-Verbose -Message "Comparing similar files..."
 $differentFiles = $destinationFileList | 
 	where { $sourceFileList.Contains($_) -and $($(Get-Item $(join-Path -Path $source -ChildPath $_)) -isnot [System.IO.DirectoryInfo]) } | 
 	where { 
-		$allBytesSource = [System.IO.File]::ReadAllBytes($(join-Path -Path $source -ChildPath $_))
+		$allBytesSource      = [System.IO.File]::ReadAllBytes($(join-Path -Path $source -ChildPath $_))
 		$allBytesDestination = [System.IO.File]::ReadAllBytes($(join-Path -Path $destination -ChildPath $_))
 		!$(IsSameContent -firstFile $allBytesSource -secondFile $allBytesDestination)
 	}
+Write-Verbose -Message "Destination Files`n`t$([System.String]::Join("`n`r`t", $differentFiles))"
+Write-Verbose -Message "found $($differentFiles.Count) different files"
+Write-Verbose -Message ""
 
-	#where { Compare-Object -ReferenceObject (Get-Content $(join-Path -Path $source -ChildPath $_)) -DifferenceObject (Get-Content $(Join-Path -Path $destination -ChildPath $_)) }
-	#where { 
-	#	Write-Host $_;
-	#	$result = $(Get-Item $(join-Path -Path $source -ChildPath $_)) -isnot [System.IO.DirectoryInfo] -and `
-	#			  ($(Get-Content $(join-Path -Path $source -ChildPath $_)) -eq $(Get-Content $(Join-Path -Path $destination -ChildPath $_)))
-	#	$result
-	#}
-$differentFiles
-echo "found $($differentFiles.Count) different files"
+$filesToRemoveFileName = $(Join-Path -Path $frontendFolder -ChildPath "filesToRemove.txt" )[0]
 
+$stream = [System.IO.StreamWriter] $filesToRemoveFileName
+$filesToRemove | ForEach-Object {
+	$stream.WriteLine($_)
+}
+$stream.close()
 
-echo "Backing up destination"
+$newFiles       | % {
+	$sourceFile = $(Join-Path -Path $source -ChildPath $_)
+	$destinationFile = $(Join-Path -Path $frontendFolder -ChildPath $_)[0]
+	copy-File-creating-folder-if-needed -source $sourceFile -destination $destinationFile
+}
+$differentFiles | % {
+	$sourceFile = $(Join-Path -Path $source -ChildPath $_)
+	$destinationFile = $(Join-Path -Path $frontendFolder -ChildPath $_)[0]
+	copy-File-creating-folder-if-needed -source $sourceFile -destination $destinationFile
+}
